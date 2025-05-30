e7e41786928b6ea794a3d1883e678cfee35e22cf
formalsarajiang <141250078+formalsarajiang@users.noreply.github.com>
2025-05-30 12:18:22 -0500

    Create Bins.java

---
+++ Bins.java
@@ -0,0 +1,18 @@
+public class Bins {
+    /**
+     * totalWastedSpace: This function takes an array of integers, representing
+     * the number of items in each shipment, and a bin size, and computes the
+     * total wasted space across all shipments.
+     *
+     * Arguments:
+     *   shipments: an array of integers
+     *   binSize: the capacity of each bin (all bins have the same capacity)
+     *
+     * Returns: The total wasted space across all shipments.
+     **/
+    public static int totalWastedSpace(int[] shipments, int binSize) {
+        // Your code here
+        // The return is included to allow the skeleton code to compile.
+        return -1;
+    }
+}
