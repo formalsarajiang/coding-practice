d69f16b35027b9404a81d977c27286f155a0e1a1
formalsarajiang <141250078+formalsarajiang@users.noreply.github.com>
2025-05-31 14:28:41 -0500

    Update Expand.java
    adding skeleton code

--- Expand.java
+++ Expand.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Expand {
+    /*
+     * Creates a list of integers from a lower bound to an upper bound,
+     * inclusive on both sides, excluding any value that is a divisor
+     * of a value in the list of values to skip.
+     * 
+     * Inputs:
+     *     lb: lower bound
+     *     up: upper bound
+     *     skip: list of multiples to skip
+     * 
+     * Returns: List of integers from lb to ub (inclusive) 
+     * except for any integer that is a divisor of a value in skip.
+     */
+    public static List<Integer> expand(int lb, int ub, List<Integer> skip) {
+        assert lb >= 2;
+        assert lb <= ub;
+
+        // TODO: Implement this function.
+        return new ArrayList<>();
+    }
+}
